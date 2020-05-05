79342b4b5e63ca853b0a3ec894bd660ce0397a88
MuhammadIbnu <m.ibnu96@gmail.com>
2020-05-05 23:49:16 +0700

    testing

--- resources/views/aktivitas/show.blade.php
+++ resources/views/aktivitas/show.blade.php
@@ -39,56 +39,56 @@
                                 <div class="row">
                                         <div class="col-md-4">
                                             <div class="thumbnail">
-                                                <a href="{{asset('public/'.$aktivitas->ktp_meninggal)}}" target="_blank">
-                                                <img src="{{asset('public/'.$aktivitas->ktp_meninggal)}}" alt="Lights" style="width:100%">
+                                                <a href="{{asset('public/uploads/berkas/'.$aktivitas->ktp_meninggal)}}" target="_blank">
+                                                <img src="{{asset('public/uploads/berkas/'.$aktivitas->ktp_meninggal)}}" alt="Lights" style="width:100%">
                                                 </a>
                                             </div>
                                         </div>
 
                                         <div class="col-md-4">
                                             <div class="thumbnail">
-                                                <a href="{{asset('public/'.$aktivitas->kk_meninggal)}}" target="_blank">
-                                                    <img src="{{asset('public/'.$aktivitas->kk_meninggal)}}" alt="Nature" style="width:100%">
+                                                <a href="{{asset('public/uploads/berkas/'.$aktivitas->kk_meninggal)}}" target="_blank">
+                                                    <img src="{{asset('public/uploads/berkas/'.$aktivitas->kk_meninggal)}}" alt="Nature" style="width:100%">
                                                 </a>
                                             </div>
                                         </div>
 
                                         <div class="col-md-4">
                                             <div class="thumbnail">
-                                                <a href="{{asset('public/'.$aktivitas->jamkesmas)}}" target="_blank">
-                                                    <img src="{{asset('public/'.$aktivitas->jamkesmas)}}" alt="Fjords" style="width:100%">
+                                                <a href="{{asset('public/uploads/berkas/'.$aktivitas->jamkesmas)}}" target="_blank">
+                                                    <img src="{{asset('public/uploads/berkas/'.$aktivitas->jamkesmas)}}" alt="Fjords" style="width:100%">
                                                 </a>
                                             </div>
                                         </div>
 
                                         <div class="col-md-4">
                                             <div class="thumbnail">
-                                            <a href="{{asset('public/'.$aktivitas->ktp_waris)}}" target="_blank">
-                                                <img src="{{asset('public/'.$aktivitas->ktp_waris)}}" alt="Fjords" style="width:100%">
+                                            <a href="{{asset('public/uploads/berkas/'.$aktivitas->ktp_waris)}}" target="_blank">
+                                                <img src="{{asset('public/uploads/berkas/'.$aktivitas->ktp_waris)}}" alt="Fjords" style="width:100%">
                                             </a>
                                             </div>
                                         </div>
 
                                         <div class="col-md-4">
                                             <div class="thumbnail">
-                                            <a href="{{asset('public/'.$aktivitas->kk_waris)}}" target="_blank">
-                                                <img src="{{asset('public/'.$aktivitas->kk_waris)}}" alt="Lights" style="width:100%">
+                                            <a href="{{asset('public/uploads/berkas/'.$aktivitas->kk_waris)}}" target="_blank">
+                                                <img src="{{asset('public/uploads/berkas/'.$aktivitas->kk_waris)}}" alt="Lights" style="width:100%">
                                             </a>
                                             </div>
                                         </div>
 
                                         <div class="col-md-4">
                                             <div class="thumbnail">
-                                                <a href="{{asset('public/'.$aktivitas->akta_kematian)}}" target="_blank">
-                                                    <img src="{{asset('public/'.$aktivitas->akta_kematian)}}" alt="Nature" style="width:100%">
+                                                <a href="{{asset('public/uploads/berkas/'.$aktivitas->akta_kematian)}}" target="_blank">
+                                                    <img src="{{asset('public/uploads/berkas/'.$aktivitas->akta_kematian)}}" alt="Nature" style="width:100%">
                                                 </a>
                                             </div>
                                         </div>
 
                                         <div class="col-md-4">
                                             <div class="thumbnail">
-                                                <a href="{{asset('public/'.$aktivitas->pakta_waris)}}" target="_blank">
-                                                    <img src="{{asset('public/'.$aktivitas->pakta_waris)}}" alt="Nature" style="width:100%">
+                                                <a href="{{asset('public/uploads/berkas/'.$aktivitas->pakta_waris)}}" target="_blank">
+                                                    <img src="{{asset('public/uploads/berkas/'.$aktivitas->pakta_waris)}}" alt="Nature" style="width:100%">
                                                 </a>
                                             </div>
                                         </div>
